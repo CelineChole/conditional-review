ffe6d2127ee5a0238e8f2e4b3e50f91862d5ddd1
CelineChole <github@celinechole.com>
2019-09-30 16:03:20 -0400

    add build a robot

---
+++ buildARobot.js
@@ -0,0 +1,6 @@
+// Build a robot
+
+// If the number is less than 10, log ‘Too easy for me’ and nothing else.​
+// If the number is even, log ‘Even numbers are my favorite’.​
+// If the number is more than 100 but less than 9000, log ‘Great, let’s have some fun’.​
+// If the number is over 9000, log ‘Sorry, that’s too big for me’ and nothing else.​
