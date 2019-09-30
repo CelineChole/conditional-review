66957954387be20b136c33bdb623cf454cc1913d
CelineChole <github@celinechole.com>
2019-09-30 16:03:29 -0400

    add ternary

---
+++ ternary.js
@@ -0,0 +1,9 @@
+let favoriteLanguage = "javascript"
+
+if ( favoriteLanguage === "javascript") {
+    console.log("you are awesome") 
+} else {
+    console.log("it's ok")
+}
+
+// Write the same code using a ternary operator
