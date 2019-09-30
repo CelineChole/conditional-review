8b79a3210c40dd282804a7577dd6e0224e4088ca
CelineChole <github@celinechole.com>
2019-09-30 16:03:25 -0400

    add debug code

---
+++ debugTheCode.js
@@ -0,0 +1,7 @@
+// Debug the code
+
+if (mont === 'July' || month === 'August') {
+    console.log('Enjoy the sun')
+} else if (month == 'January' || month == 'February' || month = march) {
+    console.log('Stay warm')
+}
