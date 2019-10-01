b3693ee66526f5b5db445e9434e4565c9c77a69d
CelineChole <github@celinechole.com>
2019-10-01 15:45:06 -0400

    add debug code

--- debugTheCode.js
+++ debugTheCode.js
@@ -1,5 +1,13 @@
 // Debug and format the below code
 
+if (mont === 'July' || month === 'August') {
+    console.log('Enjoy the sun')
+} else if (month == 'January' || month == 'February' || month = march) {
+    console.log('Stay warm')
+}
+
+// Solution
+
 let month = "January"
 
 if (month === "July" || month === "August") {
